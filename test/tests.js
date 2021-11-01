756766c8bcae4d5fd81e452d82e71ebd7fa0e271
“Robert <ring@mparticle.com>
2021-11-01 15:48:44 -0400

    test: Add enhanced conversion test for commerce event

--- test/tests.js
+++ test/tests.js
@@ -898,12 +898,82 @@ describe('Adwords forwarder', function () {
                     true
                 );
             });
+            beforeEach(function() {
+                window.enhanced_conversion_data = {};
+            })
 
-            it('should set enhanced conversions', function(done) {
+            it('should set enhanced conversion data on custom events', function(done) {
                 var successMessage = mParticle.forwarder.process({
                     EventName: 'Homepage',
                     EventDataType: MessageType.PageEvent,
                     EventCategory: EventType.Navigation,
+                    CustomFlags: {
+                        'GoogleAds.ECData': {
+                            email: 'test@gmail.com',
+                            phone_number: '1-911-867-5309',
+                            first_name: 'John',
+                            last_name: 'Doe',
+                            home_address: {
+                                street: '123 Main St',
+                                city: 'San Francisco',
+                                region: 'CA',
+                                postal_code: '12345',
+                                country: 'US',
+                            },
+                        },
+                    },
+                });
+
+                window.enhanced_conversion_data.email.should.equal(
+                    'test@gmail.com'
+                );
+                window.enhanced_conversion_data.phone_number.should.equal(
+                    '1-911-867-5309'
+                );
+                window.enhanced_conversion_data.first_name.should.equal('John');
+                window.enhanced_conversion_data.last_name.should.equal('Doe');
+                window.enhanced_conversion_data.home_address.street.should.equal(
+                    '123 Main St'
+                );
+                window.enhanced_conversion_data.home_address.city.should.equal(
+                    'San Francisco'
+                );
+                window.enhanced_conversion_data.home_address.region.should.equal(
+                    'CA'
+                );
+                window.enhanced_conversion_data.home_address.postal_code.should.equal(
+                    '12345'
+                );
+                window.enhanced_conversion_data.home_address.country.should.equal('US');
+
+                done();
+            });
+
+            it('should set enhanced conversion data on commerce events', function(done) {
+                var successMessage = mParticle.forwarder.process({
+                    EventName: 'eCommerce - Purchase',
+                    EventDataType: MessageType.Commerce,
+                    ProductAction: {
+                        ProductActionType: ProductActionType.Purchase,
+                        ProductList: [
+                            {
+                                Sku: '12345',
+                                Name: 'iPhone 6',
+                                Category: 'Phones',
+                                Brand: 'iPhone',
+                                Variant: '6',
+                                Price: 400,
+                                CouponCode: null,
+                                Quantity: 1,
+                            },
+                        ],
+                        TransactionId: 123,
+                        Affiliation: 'my-affiliation',
+                        TotalAmount: 450,
+                        TaxAmount: 40,
+                        ShippingAmount: 10,
+                    },
+                    CurrencyCode: 'USD',
                     CustomFlags: {
                         'GoogleAds.ECData': {
                             email: 'test@gmail.com',
