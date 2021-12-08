cefdc4b17a8c6211f1d71b9f8f71e8fc950aeb01
Robert Ing <robertming@gmail.com>
2021-12-08 10:43:13 -0500

    ci: Simplify test framework, update workflows (#27)
    Fixes a CI issue where Github Actions gave a positive check mark to failed tests

--- test/karma.config.js
+++ test/karma.config.js
@@ -1,13 +1,14 @@
 module.exports = function(config) {
     config.set({
         frameworks: ['mocha', 'chai'],
-        files: ['../test-bundle.js'],
+        files: ['./test-bundle.js'],
         reporters: ['progress'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         browsers: ['ChromeHeadless', 'FirefoxHeadless'],
         autoWatch: false,
+        singleRun: true,
         concurrency: Infinity,
         customLaunchers: {
             FirefoxHeadless: {
